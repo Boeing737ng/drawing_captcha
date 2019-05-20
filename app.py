59f20a5c76996ae957c374c3a2be5d245ea8cf99
Boeing737ng <ghlove0317@naver.com>
2019-05-20 14:23:09 +0900

    Signup router added

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def main():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
