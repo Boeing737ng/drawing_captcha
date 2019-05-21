dd0a313e5384e2b8a2a1df17c33c893e46e02fe9
Kyungbin Ryu <rkbin@hayanmind.com>
2019-05-21 14:27:14 +0900

    Add googleCaptcha route

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def signup():
 @app.route('/main')
 def main():
     return render_template('main.html')
+    
+@app.route('/googleCaptcha')
+def googleCaptcha():
+    return render_template('googleCaptcha.html')
 
 if __name__ == '__main__':
     app.run()
