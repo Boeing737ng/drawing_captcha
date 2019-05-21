493169dbc5cfa05e5000fab3e22cea179b429980
Boeing737ng <ghlove0317@naver.com>
2019-05-21 14:13:14 +0900

    New router added

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def login():
     return render_template('login.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
