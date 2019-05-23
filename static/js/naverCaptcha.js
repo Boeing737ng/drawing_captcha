4aee1598403f533311bcc0e53bb5fc28990cb6e2
Jaesung Na <na-0003@hotmail.com>
2019-05-23 19:06:34 +0900

    url changed

--- static/js/naverCaptcha.js
+++ static/js/naverCaptcha.js
@@ -5,7 +5,8 @@ var endTimeStamp = 0;
 var timeTaken = [];
 var typeOfCaptcha="naver";
 // var URL='http://127.0.0.1:5000/';
-var URL='http://0.0.0.0:80/';
+// var URL='http://0.0.0.0:80/';
+var URL='http://13.209.6.91:80/';
 
 //웹이랑 모바일이랑 카운터 분리해야함
 var device = "undefined";
